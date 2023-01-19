5b03a331eada3cc31db3137856d730505af7b109
Arthur Lu <learthurgo@gmail.com>
2023-01-19 20:22:26 +0000

    add echo path, add cookie-parser to requirements
    Signed-off-by: Arthur Lu <learthurgo@gmail.com>

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-var package = require('./package.json');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser")
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+var package = require("./package.json");
 
 const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
+app.use(cookieParser())
 app.use(cors());
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 
-app.get('/api/version', (req, res) => {
+app.get("/api/version", (req, res) => {
 	res.send({version: package.version});
 });
 
+app.get("/api/echo", (req, res) => {
+	res.send({recieved: {body: req.body, cookies: req.cookies}});
+});
+
 app.listen(80, () => {
-	console.log('listening on port 80');
+	console.log("listening on port 80");
 });
