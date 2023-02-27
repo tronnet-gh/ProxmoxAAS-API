3fe69940644cc93f4fcc0075ea92a8b2e08df28c
Arthur Lu <learthurgo@gmail.com>
2023-02-27 01:09:49 +0000

    add additional prototypes in db.js

--- db.js
+++ db.js
@@ -1,3 +1,7 @@
+const fs = require("fs");
+
+filename = "localdb.json";
+
 /**
  * called at app startup, can be used to initialize any variables needed for database access
  */
@@ -7,13 +11,23 @@ function init () {}
  * user requests additional resources specified in k-v pairs
  * @param {string} user user's proxmox username in the form username@authrealm
  * @param {Object} resources k-v pairs with resource name as keys and resource ammount as values
+ * @returns {boolean} whether the user is approved to allocate requested resources
  */
 function requestResources (user, resources) {}
+
+/**
+ * user allocates additional resources specified in k-v pairs
+ * @param {string} user user's proxmox username in the form username@authrealm
+ * @param {Object} resources k-v pairs with resource name as keys and resource ammount as values
+ * @returns {boolean} true if resources were successfully allocated, false otherwise
+ */
+function allocateResources (user, resources) {}
 
 /**
  * user releases allocated resources specified in k-v pairs
  * @param {string} user user's proxmox username in the form username@authrealm
  * @param {Object} resources k-v pairs with resource name as keys and resource ammount as values
+ * @returns {boolean} true if resources were successfully deallocated, false otherwise
  */
 function releaseResources (user, resources) {}
 
