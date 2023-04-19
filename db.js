109f9993cc6081a638ab93938a06a30753057335
Arthur Lu <root@tronnet.net>
2023-04-19 20:36:48 +0000

    bug fixes
    Signed-off-by: Arthur Lu <learthurgo@gmail.com>

--- db.js
+++ db.js
@@ -36,28 +36,4 @@ function getUser (username) {
 	return db.users[username];
 }
 
-function setUsedResources (username, used) {
-	let userEntry = db.users[username];
-	userEntry.used = used;
-	userEntry.avail = {};
-	Object.keys(userEntry.max).forEach((k) => {
-		userEntry.avail[k] = userEntry.max[k] - userEntry.used[k];
-	});
-	save();
-}
-
-async function approveResources (username, request) {
-	let approved = true;
-	let avail = db.users[username].avail;
-	Object.keys(request).forEach((key) => {
-		if (!(key in avail)) {
-			approved = false;
-		}
-		else if (avail[key] - request[key] < 0) {
-			approved = false;
-		}
-	});
-	return approved;
-}
-
-module.exports = {init, getResourceMeta, getResourceUnits, getUser, setUsedResources, approveResources};
+module.exports = {init, getResourceMeta, getResourceUnits, getUser};
