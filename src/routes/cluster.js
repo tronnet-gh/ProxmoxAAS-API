e8ed9fc3b8190ec4cb2e0f6d2ebc8da8d3e0560d
Arthur Lu <root@tronnet.net>
2023-08-07 18:48:47 +0000

    dynamically import routes

--- src/routes/cluster.js
+++ src/routes/cluster.js
@@ -16,17 +16,7 @@ const vmidRegexP = "\\d+";
 
 const basePath = `/:node(${nodeRegexP})/:type(${typeRegexP})/:vmid(${vmidRegexP})`;
 
-import("./cluster/disk.js").then((module) => {
-	router.use(`${basePath}/disk`, module.router);
-});
-
-import("./cluster/net.js").then((module) => {
-	router.use(`${basePath}/net`, module.router);
-});
-
-import("./cluster/pci.js").then((module) => {
-	router.use(`${basePath}/pci`, module.router);
-});
+global.utils.recursiveImport(router, basePath, "cluster", import.meta.url);
 
 /**
  * GET - get available pcie devices given node and user
