f000582213ac15ee8a3ee11c78ae87d2e7b5cb1b
Arthur Lu <root@tronnet.net>
2024-01-22 19:47:13 +0000

    add put and delete proxmox proxy routes

--- src/routes/proxmox.js
+++ src/routes/proxmox.js
@@ -6,7 +6,6 @@ export const router = Router({ mergeParams: true }); ;
  * request and responses passed through to/from proxmox
  */
 router.get("/*", async (req, res) => { // proxy endpoint for GET proxmox api with no token
-	console.log(req.url);
 	const path = req.url.replace("/api/proxmox", "");
 	const result = await global.pve.requestPVE(path, "GET", { cookies: req.cookies });
 	res.status(result.status).send(result.data);
@@ -20,5 +19,26 @@ router.post("/*", async (req, res) => { // proxy endpoint for POST proxmox api w
 	const path = req.url.replace("/api/proxmox", "");
 	const body = JSON.parse(JSON.stringify(req.body));
 	const result = await global.pve.requestPVE(path, "POST", { cookies: req.cookies }, body); // need to stringify body because of other issues
+	res.status(result.status).send(result.data);
+});
+
+/**
+ * PUT - proxy proxmox api without privilege elevation
+ * request and responses passed through to/from proxmox
+ */
+router.put("/*", async (req, res) => { // proxy endpoint for POST proxmox api with no token
+	const path = req.url.replace("/api/proxmox", "");
+	const body = JSON.parse(JSON.stringify(req.body));
+	const result = await global.pve.requestPVE(path, "PUT", { cookies: req.cookies }, body); // need to stringify body because of other issues
+	res.status(result.status).send(result.data);
+});
+
+/**
+ * DELETE - proxy proxmox api without privilege elevation
+ * request and responses passed through to/from proxmox
+ */
+router.delete("/*", async (req, res) => { // proxy endpoint for GET proxmox api with no token
+	const path = req.url.replace("/api/proxmox", "");
+	const result = await global.pve.requestPVE(path, "DELETE", { cookies: req.cookies });
 	res.status(result.status).send(result.data);
 });
