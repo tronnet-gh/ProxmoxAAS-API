8f7ea517879b4912cf9d844b5260cfe61d03c7b4
Arthur Lu <root@tronnet.net>
2024-07-08 19:25:23 +0000

    add missing valid pve token check to checkAuth

--- src/routes/access/groups.js
+++ src/routes/access/groups.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 export const router = Router({ mergeParams: true });
 
+const checkAuth = global.utils.checkAuth;
+
 /**
  * GET - get all groups
  * responses:
@@ -8,9 +10,10 @@ export const router = Router({ mergeParams: true });
  * - 201: {auth: false}
  */
 router.get("/", async (req, res) => {
-    const auth = await checkAuth(req.cookies, res);
+	// check auth
+	const auth = await checkAuth(req.cookies, res);
 	if (!auth) {
 		return;
 	}
-    res.status(200).send(global.userManager.getAllGroups())
+	res.status(200).send(global.userManager.getAllGroups());
 });
--- src/routes/access/users.js
+++ src/routes/access/users.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 export const router = Router({ mergeParams: true });
 
+const checkAuth = global.utils.checkAuth;
+
 /**
  * GET - get all users
  * responses:
@@ -8,9 +10,10 @@ export const router = Router({ mergeParams: true });
  * - 201: {auth: false}
  */
 router.get("/", async (req, res) => {
-    const auth = await checkAuth(req.cookies, res);
+	// check auth
+	const auth = await checkAuth(req.cookies, res);
 	if (!auth) {
 		return;
 	}
-    res.status(200).send(global.userManager.getAllUsers())
+	res.status(200).send(global.userManager.getAllUsers());
 });
